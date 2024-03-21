4f209e23284b25063ce45408c7174eb4ba246cf2
horpuma <horpuma@gmail.com>
2024-03-20 21:57:12 -0400

    Function Producto mas swagger terminado

---
+++ APIexamen1HoracioPuma/APIexamen1HoracioPuma/Modelo/Implementacion/Producto.cs
@@ -0,0 +1,24 @@
+﻿using APIexamen1HoracioPuma.Modelo.Contratos;
+using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIexamen1HoracioPuma.Modelo.Implementacion
+{
+    public class Producto : IProducto, ITableEntity
+    {
+        public string Id { get ; set ; }
+        public string Nombre { get ; set ; }
+        public double Precio { get ; set ; }
+        public string ProveedorId { get ; set ; }
+        //Variables TableEntity
+        public string PartitionKey { get ; set ; }
+        public string RowKey { get ; set ; }
+        public DateTimeOffset? Timestamp { get ; set ; }
+        public ETag ETag { get ; set ; }
+    }
+}
